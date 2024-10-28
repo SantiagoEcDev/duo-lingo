f91046ad76d6a656713b2687ca543ce22e60ed1f
SantiagoEcDev <santiagocorredor51@hotmail.com>
2024-10-28 16:49:37 -0500

    add units

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -8,41 +8,107 @@ const sql = neon(process.env.DATABASE_URL!);
 
 const db = drizzle(sql, { schema });
 
-const main = async() => {
-    try {
-        console.log("Seeding database")
+const main = async () => {
+  try {
+    console.log("Seeding database");
 
-        await db.delete(schema.courses);
-        await db.delete(schema.userProgress);
+    await db.delete(schema.courses);
+    await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
 
-        await db.insert(schema.courses).values([{
-            id: 1,
-            title: "Spanish",
-            imageSrc: "/es.svg"
-        },
-        {
-            id: 2,
-            title: "English",
-            imageSrc: "/en.svg"
-        },
-        {
-            id: 3,
-            title: "Italian",
-            imageSrc: "/it.svg"
-        },
-        {
-            id: 4,
-            title: "French",
-            imageSrc: "/fr.svg"
-        },])
+    await db.insert(schema.courses).values([
+      {
+        id: 1,
+        title: "Spanish",
+        imageSrc: "/es.svg",
+      },
+      {
+        id: 2,
+        title: "English",
+        imageSrc: "/en.svg",
+      },
+      {
+        id: 3,
+        title: "Italian",
+        imageSrc: "/it.svg",
+      },
+      {
+        id: 4,
+        title: "French",
+        imageSrc: "/fr.svg",
+      },
+    ]);
 
-        console.log("Seeding finished");
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 1,
+        title: "Unit 1",
+        description: "Learn the basics of spanish",
+        order: 1,
+      },
+    ]);
 
-    }catch (error) {
-        console.error(error)
-        throw new Error("Failed to seed the database")
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1,
+        order: 1,
+        title: "Nouns",
+      },
+      {
+        id: 2,
+        unitId: 1,
+        order: 2,
+        title: "Verbs",
+      },
+    ]);
 
-    }
-}
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1,
+        type: "SELECT",
+        order: 1,
+        question: 'Which one of these is "the man"?',
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1,
+        imageSrc: "/man.svg",
+        correct: true,
+        text: "el hombre",
+        audioSrc: "/es_man.mp3",
+      },
+      {
+        id: 3,
+        challengeId: 1,
+        imageSrc: "/woman.svg",
+        correct: false,
+        text: "la mujer",
+        audioSrc: "/es_woman.mp3",
+      },
+      {
+        id: 4,
+        challengeId: 1,
+        imageSrc: "/roboto.svg",
+        correct: false,
+        text: "el robot",
+        audioSrc: "/es_robot.mp3",
+      },
+    ]);
+
+    console.log("Seeding finished");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to seed the database");
+  }
+};
 
 main();
