a29723858885e484908fc26a1fa34f80c6040fb4
SantiagoEcDev <santiagocorredor51@hotmail.com>
2024-10-28 15:09:42 -0500

    add lingo schema

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -8,27 +8,24 @@ import { redirect } from "next/navigation";
 const LearnPage = async () => {
   const userProgressData = getUserProgress();
 
-  const [userProgress] = await Promise.all([userProgressData])
+  const [userProgress] = await Promise.all([userProgressData]);
 
-  if(!userProgress || !userProgress.activeCourse){
-    redirect("/courses")
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
   }
 
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{ title: userProgress.activeCourse.title, imageSrc: userProgress.activeCourse.imageSrc }}
-          hearts={3}
-          points={100}
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
           hasActiveSubscription={false}
         />
       </StickyWrapper>
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
-        <div className="space-y-4">
-          
-        </div>
       </FeedWrapper>
     </div>
   );
